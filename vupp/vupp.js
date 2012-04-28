1942eefedd637b157f648f10f2b562237247561d
Olav Schettler <olav@schettler.net>
2012-04-28 20:28:57 +0200

    Portiert nach Drupal 7

--- vupp/vupp.js
+++ vupp/vupp.js
@@ -1,27 +1,26 @@
 // $Id$
 
-$(document).ready(function() {
-	$('a.vupp.active').each(function () {
-		// Read in the path to the PHP handler
-		var uri = $(this).attr('href');
-		$(this).removeAttr('href'); 
-		var cid = $(this).attr('id');
-		var pid = 'vupp-points-' + cid.match(/[0-9]+$/);
-		$(this).click(function() {
-			// Ajax GET request for vote data
-			$.ajax({
-				type: "GET",
-				    url: uri,
-				    success: function (data) {
-				    $('#' + cid).removeClass('active');
-				    // extract the cid so we can change other elements for the same cid
-				    // update the points
-				    $('#' + pid).html(data);
-				},
-				    error: function (xmlhttp) {
-				    alert('An HTTP error '+ xmlhttp.status +' occured.\n'+ db.uri);
-				}
-			    });
-		    });
-	    });
+jQuery(function($) {
+  $('a.vupp.active').click(function () {
+    // Read in the path to the PHP handler
+    var uri = $(this).attr('href');
+    $(this).removeAttr('href'); 
+    var cid = $(this).attr('id');
+    var pid = 'vupp-points-' + cid.match(/[0-9]+$/);
+    // Ajax GET request for vote data
+    $.ajax({
+      type: "GET",
+      url: uri,
+      success: function (data) {
+        $('#' + cid).removeClass('active');
+        // extract the cid so we can change other elements for the same cid
+        // update the points
+        $('#' + pid).html(data);
+      },
+      error: function (xmlhttp) {
+        alert('An HTTP error '+ xmlhttp.status +' occured.\n'+ db.uri);
+      }
     });
+    return false;
+  });
+});
